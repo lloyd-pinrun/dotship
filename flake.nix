6997544a6a43ffd400a62365f0876701ead283a0
Tristan Schrader <t0rdos@pm.me>
2024-04-02 12:36:03 -0700

    bump: pin nixpkgs because of xz exploit

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "Useful flake-parts modules";
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    # nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    # Pinning this while xz exploit is being resolved in unstable stream
+    nixpkgs.url = github:nixos/nixpkgs/f72123158996b8d4449de481897d855bc47c7bf6;
     nixpkgs-stable.url = github:nixos/nixpkgs/nixos-23.11;
     systems.url = github:nix-systems/default;
 
