be0937e170fc9b70baf3160d0600eb0002db466c
Tristan Schrader <t0rdos@pm.me>
2024-04-03 12:36:06 -0700

    bump: back to nixpkgs/nixos-unstable for xz revert

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "Useful flake-parts modules";
   inputs = {
-    # nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    # Pinning this while xz exploit is being resolved in unstable stream
-    nixpkgs.url = github:nixos/nixpkgs/f72123158996b8d4449de481897d855bc47c7bf6;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
     nixpkgs-stable.url = github:nixos/nixpkgs/nixos-23.11;
     systems.url = github:nix-systems/default;
 
