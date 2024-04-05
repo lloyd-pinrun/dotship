496f9e0918516c93a6b4b6479229e7d8677d4f39
Tristan Schrader <t0rdos@pm.me>
2024-04-04 18:49:14 -0700

    feat: config option for nixpkgs pkgs arg

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,19 +1,26 @@
 {
+  config,
   inputs,
   lib,
   ...
 }:
 with lib; {
-  flake.overlays.fromYAML = _: prev: {
+  options.canivete.pkgs.config = mkOption {
+    type = with types; attrsOf anything;
+    default = {};
+    description = mdDoc "Nixpkgs configuration (i.e. allowUnfree, etc.)";
+  };
+  config.flake.overlays.fromYAML = _: prev: {
     fromYAML = flip pipe [
       (file: "${prev.yq}/bin/yq '.' ${file} > $out")
       (prev.runCommand "from-yaml" {})
       importJSON
     ];
   };
-  perSystem = {system, ...}: {
+  config.perSystem = {system, ...}: {
     _module.args.pkgs = import inputs.nixpkgs {
       inherit system;
+      inherit (config.canivete.pkgs) config;
       overlays = attrValues inputs.self.overlays;
     };
   };
