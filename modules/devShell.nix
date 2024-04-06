b9c25d1f9b5f5902d0a93d03b5f49d6d2a454e61
Tristan Schrader <t0rdos@pm.me>
2024-04-06 12:49:58 -0700

    feat: name the devshell parent wrapper exe

--- modules/devShell.nix
+++ modules/devShell.nix
@@ -1,12 +1,30 @@
 {
-  perSystem = {
-    lib,
+  config,
+  nix,
+  ...
+}:
+with nix; {
+  options.canivete.devShell.name = mkOption {
+    type = str;
+    default = "can";
+    description = mdDoc "Name of the primary project executable";
+  };
+  config.perSystem = {
     pkgs,
     self',
     ...
   }: {
+    packages.default = pkgs.writeShellApplication {
+      name = config.canivete.devShell.name;
+      excludeShellChecks = ["SC2015"];
+      text = ''
+        nixCmd() { nix --extra-experimental-features "nix-command flakes" "$@"; }
+        [[ -z ''${1-} || $1 == default ]] && nixCmd flake show || nixCmd run ".#$1" -- "''${@:2}"
+      '';
+    };
     devShells.default = pkgs.mkShell {
-      inputsFrom = lib.attrValues (removeAttrs self'.devShells ["default"]);
+      inputsFrom = attrValues (removeAttrs self'.devShells ["default"]);
+      packages = [self'.packages.default];
     };
   };
 }
