8def880ddf747b9183dd9031c4da8bdde898c06f
Tristan Schrader <t0rdos@pm.me>
2024-04-16 14:43:08 -0700

    feat: add common nix flags to "can"

--- modules/devShell.nix
+++ modules/devShell.nix
@@ -18,7 +18,11 @@ with nix; {
       inherit (config.canivete.devShell) name;
       excludeShellChecks = ["SC2015"];
       text = ''
-        nixCmd() { nix --extra-experimental-features "nix-command flakes" "$@"; }
+        nixCmd() {
+          nix --show-trace --allow-import-from-derivation \
+            --extra-experimental-features "nix-command flakes auto-allocate-uids configurable-impure-env" \
+            "$@"
+        }
         [[ -z ''${1-} || $1 == default ]] && nixCmd flake show || nixCmd run ".#$1" -- "''${@:2}"
       '';
     };
