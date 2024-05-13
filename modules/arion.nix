6b5abb453a499eaf029ba8e636374a782b989204
Tristan Schrader <t0rdos@pm.me>
2024-05-13 08:54:22 -0700

    refactor: fix: arion under perSystem with pkgs

--- modules/arion.nix
+++ modules/arion.nix
@@ -1,22 +1,25 @@
 {
-  config,
   inputs,
   nix,
   ...
 }:
 with nix; {
-  options.canivete.arion.modules = mkModulesOption {};
-  config.canivete.arion.modules.name.project.name = mkDefault config.canivete.devShell.name;
-  config.perSystem = {
+  options.perSystem = mkPerSystemOption ({
+    config,
     inputs',
     pkgs,
     ...
-  }: let
-    docker-compose-yaml = inputs.arion.lib.build {modules = attrValues config.canivete.arion.modules;};
-  in {
-    packages.arion = pkgs.writeShellApplication {
-      name = "arion";
-      text = "${getExe inputs'.arion.packages.arion} --prebuilt-file ${docker-compose-yaml} \"$@\"";
-    };
-  };
+  }: {
+    options.canivete.arion.modules = mkModulesOption {};
+    config.canivete.arion.modules.name.project.name = mkDefault config.canivete.devShell.name;
+    config.apps.arion = let
+      modules = attrValues config.canivete.arion.modules;
+      docker-compose-yaml = inputs.arion.lib.build {inherit modules pkgs;};
+      script = pkgs.writeShellApplication {
+        name = "arion";
+        text = "${getExe inputs'.arion.packages.arion} --prebuilt-file ${docker-compose-yaml} \"$@\"";
+      };
+    in
+      mkApp script;
+  });
 }
