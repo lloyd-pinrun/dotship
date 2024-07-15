632500b9c1b717ed717ee27fac6896cd09aab184
Tristan Schrader <t0rdos@pm.me>
2024-07-15 15:10:59 -0700

    feat: pass self' of linux system variant to arion
    additional flexibility for using flake packages

--- modules/arion.nix
+++ modules/arion.nix
@@ -7,19 +7,22 @@ with nix; {
   options.perSystem = mkPerSystemOption ({
     config,
     flake,
-    pkgs,
     system,
     ...
-  }: {
+  }: let
+    # Containers rely on the Linux kernel, so for this to work on a Darwin client, configure distributed builds
+    system'' = replaceStrings ["darwin"] ["linux"] system;
+  in {
     options.canivete.arion.modules = mkModulesOption {};
-    config.canivete.arion.modules.name.project.name = mkDefault config.canivete.devShell.name;
+    config.canivete.arion.modules.builtin = {
+      # Also share self' of the Linux system variant
+      _module.args.self'' = flake.config.perInput system'' inputs.self;
+      project.name = mkDefault config.canivete.devShell.name;
+    };
     config.canivete.devShell.apps.arion.script = let
+      inherit (flake.config.canivete.${system''}.pkgs) pkgs;
       modules = attrValues config.canivete.arion.modules;
-      docker-compose-yaml = inputs.arion.lib.build {
-        inherit modules;
-        # Containers rely on the Linux kernel, so for this to work on a Darwin client, configure distributed builds
-        inherit (flake.config.canivete.${replaceStrings ["darwin"] ["linux"] system}.pkgs) pkgs;
-      };
+      docker-compose-yaml = inputs.arion.lib.build {inherit modules pkgs;};
     in "${getExe inputs.arion.packages.${system}.arion} --prebuilt-file ${docker-compose-yaml} \"$@\"";
   });
 }
