77d84848467b9e444c1de5d6750b00a9c830f7b5
Tristan Schrader <t0rdos@pm.me>
2024-07-16 22:17:27 -0700

    fix: archive arion build on macos
    streamLayeredImage will generate a script with shebangs for the system
    used to generate the actual image, which means it can't be run on a host
    with a different system. this is a the situation of an M1 macOS using
    linux-builder. the workaround is to force buildLayeredImage (will be
    fully built on the linux-builder and passed to docker-compose directly)
    when the image system does not match the host system.

--- modules/arion.nix
+++ modules/arion.nix
@@ -6,22 +6,35 @@ flake @ {
 with nix; {
   options.perSystem = mkPerSystemOption ({
     config,
+    pkgs,
     system,
     ...
   }: let
+    # Arion has no argument to prefer buildLayeredImage when streamLayeredImage doesn't work across systems
+    arion-patched = pkgs.applyPatches {
+      name = "arion-patched-src";
+      src = inputs.arion;
+      patches = [./arion.patch];
+    };
     # Containers rely on the Linux kernel, so for this to work on a Darwin client, configure distributed builds
     system'' = replaceStrings ["darwin"] ["linux"] system;
   in {
     options.canivete.arion.modules = mkModulesOption {};
     config.canivete.arion.modules.builtin = {
       # Also share self' of the Linux system variant
-      _module.args.self'' = flake.config.perInput system'' inputs.self;
-      project.name = mkDefault config.canivete.devShell.name;
+      config._module.args.self'' = flake.config.perInput system'' inputs.self;
+      config.project.name = mkDefault config.canivete.devShell.name;
+      # Mismatched systems should buildLayeredImage to allow running on host
+      options.services = mkOption {
+        type = attrsOf (submodule {
+          config.image.asStream = system == system'';
+        });
+      };
     };
     config.canivete.devShell.apps.arion.script = let
       inherit (inputs.self.canivete.${system''}.pkgs) pkgs;
       modules = attrValues config.canivete.arion.modules;
-      docker-compose-yaml = inputs.arion.lib.build {inherit modules pkgs;};
-    in "${getExe inputs.arion.packages.${system}.arion} --prebuilt-file ${docker-compose-yaml} \"$@\"";
+      docker-compose-yaml = arion-patched.lib.build {inherit modules pkgs;};
+    in "${getExe arion-patched.packages.${system}.arion} --prebuilt-file ${docker-compose-yaml} \"$@\"";
   });
 }
