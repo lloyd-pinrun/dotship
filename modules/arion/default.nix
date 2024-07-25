d674a025b77d7644e1d08ed38a8c12583da88dd9
Tristan Schrader <t0rdos@pm.me>
2024-07-25 12:13:48 -0700

    feat: arion module options for packages

--- modules/arion/default.nix
+++ modules/arion/default.nix
@@ -18,8 +18,30 @@ with nix; {
     };
     # Containers rely on the Linux kernel, so for this to work on a Darwin client, configure distributed builds
     system'' = replaceStrings ["darwin"] ["linux"] system;
+
+    # Module shorthand
+    cfg = config.canivete.arion;
+    inherit (inputs.self.canivete.${system''}.pkgs) pkgs;
+    modules = attrValues cfg.modules;
   in {
-    options.canivete.arion.modules = mkModulesOption {};
+    options.canivete.arion = {
+      modules = mkModulesOption {};
+      yaml = mkOption {
+        type = package;
+        description = "docker-compose YAML output";
+        default = arion-patched.lib.build {inherit modules pkgs;};
+      };
+      basePackage = mkOption {
+        type = package;
+        description = "Base arion package to use";
+        default = arion-patched.packages.${system}.arion;
+      };
+      finalPackage = mkOption {
+        type = package;
+        description = "Final arion executable";
+        default = pkgs.wrapProgram cfg.basePackage "arion" "arion" "--add-flags \"--prebuilt-file ${cfg.yaml}\"" {};
+      };
+    };
     config.canivete.arion.modules.builtin = {
       # Also share self' of the Linux system variant
       config._module.args.self'' = flake.config.perInput system'' inputs.self;
