b831d32d50bceac6e74ee8eb8023794f30d0d09a
Tristan Schrader <t0rdos@pm.me>
2024-07-25 15:15:51 -0700

    feat: expose "nix" object at toplevel lib

--- flake.nix
+++ flake.nix
@@ -106,19 +106,23 @@
         imports = [./modules];
         _module.args.nix = nix;
         flake = {
-          lib.mkFlake = with nix;
-            args @ {
-              specialArgs ? {},
-              everything ? [],
-              ...
-            }: module:
-              flake-parts.lib.mkFlake {
-                inputs = inputs // args.inputs;
-                specialArgs = {inherit nix;} // specialArgs;
-              } {
-                imports = concat [module self.flakeModule] (nix.filesets.nix.everything everything);
-                perSystem._module.args.nix = nix;
-              };
+          lib =
+            nix
+            // {
+              mkFlake = with nix;
+                args @ {
+                  specialArgs ? {},
+                  everything ? [],
+                  ...
+                }: module:
+                  flake-parts.lib.mkFlake {
+                    inputs = inputs // args.inputs;
+                    specialArgs = {inherit nix;} // specialArgs;
+                  } {
+                    imports = concat [module self.flakeModule] (nix.filesets.nix.everything everything);
+                    perSystem._module.args.nix = nix;
+                  };
+            };
           flakeModule = config.flake.flakeModules.default;
           flakeModules = with nix;
             pipe ./modules [
