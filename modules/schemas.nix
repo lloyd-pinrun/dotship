28746239333059d3c2fa4ffe3cb7cc926d37855b
Tristan Schrader <t0rdos@pm.me>
2024-08-20 16:26:02 -0700

    feat: anything freeformType for canivete. options

--- modules/schemas.nix
+++ modules/schemas.nix
@@ -5,20 +5,26 @@
   ...
 }:
 with nix; {
-  flake.inputs = inputs;
-  flake.canivete = mergeAttrs config.canivete (mapAttrs (_: getAttr "canivete") config.allSystems);
-  flake.schemas =
-    inputs.flake-schemas.schemas
-    // {
-      flakeModules = {
-        version = 1;
-        doc = "flake-parts modules";
-        inventory = output: {
-          children = flip mapAttrs output (_: module: {
-            what = "flake-parts module";
-            evalChecks.isModule = isAttrs module || (isFunction module && (functionArgs module) ? flake-parts-lib);
-          });
+  options.canivete = mkOption {
+    type = submodule {freeformType = attrsOf anything;};
+    default = {};
+  };
+  config.flake = {
+    inherit inputs;
+    canivete = mergeAttrs config.canivete (mapAttrs (_: getAttr "canivete") config.allSystems);
+    schemas =
+      inputs.flake-schemas.schemas
+      // {
+        flakeModules = {
+          version = 1;
+          doc = "flake-parts modules";
+          inventory = output: {
+            children = flip mapAttrs output (_: module: {
+              what = "flake-parts module";
+              evalChecks.isModule = isAttrs module || (isFunction module && (functionArgs module) ? flake-parts-lib);
+            });
+          };
         };
       };
-    };
+  };
 }
