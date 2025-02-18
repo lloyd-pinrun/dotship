01e8f200e3f8c10de0dab1599d7376c4b0e150c3
Tristan Schrader <t0rdos@pm.me>
2025-02-18 13:34:34 -0800

    fix: update template to project refactor

--- template/flake.nix
+++ template/flake.nix
@@ -1,12 +1,8 @@
 {
   inputs.canivete.url = github:schradert/canivete;
-  outputs = inputs:
-    with inputs;
-      canivete.lib.mkFlake {
-        inherit inputs;
-        # Any path added to `everything` will treat every matching .nix file recursively as a flake-parts module
-        everything = [];
-      } {
-        # This is a flake-parts module with canivete options supported
-      };
+  # Argumenmts are:
+  # 1. flake-parts module args
+  # 2. directories where every .nix file recursively is a flake-parts module
+  # 3. root flake-parts module
+  outputs = inputs: inputs.canivete.lib.mkFlake {inherit inputs;} [] {};
 }
