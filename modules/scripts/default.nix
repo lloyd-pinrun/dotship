e77a55f0d568a4137550a54063fbeadcb79afef8
Tristan Schrader <t0rdos@pm.me>
2025-02-18 13:33:26 -0800

    feat: module for scripts under canivete

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -1,7 +1,14 @@
 {
-  flake.canivete.scripts = {
-    utils = ./utils.sh;
-    sops-encrypt = ./encrypt.sh;
-    sops-setup = ./setup.sh;
+  perSystem = {lib, pkgs, ...}: {
+    options.canivete.scripts = lib.mkOption {
+      default = {};
+      description = "Scripts!";
+      type = with lib.types; attrsOf (coercedTo pathInStore (pkgs.writeShellScriptBin "canivete") package);
+    };
+    config.canivete.scripts = {
+      utils = ./utils.sh;
+      sops-encrypt = ./encrypt.sh;
+      sops-setup = ./setup.sh;
+    };
   };
 }
