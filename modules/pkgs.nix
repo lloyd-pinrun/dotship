da245b8af707a96ad21de79b292db37239e71951
Tristan Schrader <t0rdos@pm.me>
2025-02-23 02:57:51 -0800

    feat(pkgs): allowUnfree option

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -4,14 +4,25 @@
   lib,
   ...
 }: let
-  inherit (lib) attrValues flip pipe importJSON toList getExe mkOption types;
+  inherit (lib) attrValues flip pipe importJSON toList mkIf getExe mkOption types elem getName;
+  inherit (types) listOf str attrsOf anything;
 in {
   # TODO options for overlays (from options.flake.overlays doesn't work?)
   # TODO nixpkgs config options
-  options.canivete.pkgs.config = mkOption {
-    type = types.attrsOf types.anything;
-    default = {};
-    description = "Nixpkgs configuration (i.e. allowUnfree, etc.)";
+  options.canivete.pkgs = {
+    allowUnfree = mkOption {
+      type = listOf str;
+      deafult = [];
+      description = "Package names to ignore because unfree";
+    };
+    config = mkOption {
+      type = attrsOf anything;
+      default = {};
+      description = "Nixpkgs configuration (i.e. allowUnfreePredicate, etc.)";
+    };
+  };
+  config.canivete.pkgs.config = mkIf (config.canivete.pkgs.allowUnfree != []) {
+    allowUnfreePredicate = pkg: elem (getName pkg) config.canivete.pkgs.allowUnfree;
   };
   config.perSystem = {
     pkgs,
