56281769274e3b01bec322a72defcd5f3ecf9f57
Cam Carter <ccprimitivo@gmail.com>
2026-01-06 18:20:48 -0600

    feat(modules/vars): set default `email` on user `profiles`

--- modules/vars.nix
+++ modules/vars.nix
@@ -1,4 +1,10 @@
-{dotlib, ...}: {
+{
+  dotlib,
+  config,
+  ...
+}: let
+  inherit (config.dotship) vars;
+in {
   options.dotship.vars = {
     domain = dotlib.options.str "deployment domain" {};
 
@@ -9,14 +15,14 @@
       options.settings = dotlib.options.raw "the settings associated with the primary super-user" {default = users.${sudoer.username};};
     });
 
-    users = dotlib.options.attrs.submodule "all users to configure for deployment" {
+    users = dotlib.options.attrs.submodule "all users to configure for deployment" ({name, ...}: {
       options.description = dotlib.options.str "the user's first & last name" {example = "Peter Griffin";};
       options.accounts = dotlib.options.attrs.str "mapping of external account names to the user" {example = {github = "pgriffin";};};
 
       options.profiles = dotlib.options.attrs.submodule "mapping of profile information to the user" {
-        options.email = dotlib.options.email "the user's email for the profile" {};
+        options.email = dotlib.options.email "the user's email for the profile" {default = "${name}@${vars.domain}";};
         options.ssh-pub-key = dotlib.options.str "the user's public SSH key for the profile" {};
       };
-    };
+    });
   };
 }
